2d91cda233f6a039b6af94faee8c003c20946232
Prachi Agarwal <33975572+Prachi0141@users.noreply.github.com>
2018-11-25 22:56:51 +0530

    Update scratch.py

--- scratch.py
+++ scratch.py
@@ -1,3 +1,5 @@
+from bs4 import BeautifulSoup
+import urllib3.request
 import cv2
 import numpy as np
 import os
@@ -5,19 +7,38 @@ import subprocess
 
 import time
 
-src = int(input("Enter a number: "))
-dest = int(input("Enter a number: "))
+ex = -1
+ey = -1
+sx = -1
+sy = -1
+k = 0
+# mouse callback function
+def draw_circle(event, x, y, flags, param):
+    global sx, sy, ex, ey, k
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        if k == 0:
+            sx = x
+            sy = y
+            k += 1
+        elif k == 1:
+            ex = x
+            ey = y
+            k += 1
+
+        cv2.circle(sdio, (x, y), 8, (255, 0, 0), -1)
+        print(x, y)
+
+
 #st = src + " " + dest
 
 start_time = time.time()
-st = str(src)+" " + str(dest)
-print(st)
+
 cmd = "dijk_path.cpp"
 #cap = cv2.VideoCapture('p_vid.mp4')
 
 #while(True):
 frame = cv2.imread('finaldrawr.jpg')
-cv2.imwrite('save.jpg', frame)
+#cv2.imwrite('save.jpg', frame)
 cv2.circle(frame, (0, 0), 5, (0, 0, 255), -1)
 cv2.circle(frame, (583, 0), 5, (0, 0, 255), -1)
 cv2.circle(frame, (583, 410), 5, (0, 0, 255), -1)
@@ -28,8 +49,23 @@ pts2 = np.float32([[0, 0], [300, 0], [300, 300], [0, 300]])
 matrix = cv2.getPerspectiveTransform(pts1, pts2)
 
 result = cv2.warpPerspective(frame, matrix, (300, 300))
-cv2.imshow('Original', frame)
-cv2.imshow('pers', result)
+sdio = result
+cv2.namedWindow('image2')
+#frame = cv.imread('finaldrawr.jpg')
+cv2.setMouseCallback('image2', draw_circle)
+
+while(True):
+    cv2.imshow('image2', sdio)
+    if cv2.waitKey(20) & 0xFF == 27:
+        break
+
+print(sx, sy, ex, ey)
+src = ((sy//10)*30) + (sx//10)
+dest = ((ey//10)*30) + (ex//10)
+st = str(src)+" " + str(dest)
+print(st)
+##cv2.imshow('Original', frame)
+#cv2.imshow('pers', result)
 frame = result
 hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
@@ -40,13 +76,13 @@ mask = cv2.inRange(hsv, lower_red, upper_red)
 res = cv2.bitwise_and(frame, frame, mask=mask)
 
 edges = cv2.Canny(frame, 200, 200)
-cv2.imshow('Edges', edges)
+##cv2.imshow('Edges', edges)
 #temp = cv2.resize(edges, (300,300))
 #temp1 = temp
-#cv2.imshow('temp', temp)
-kernel = np.ones((25, 25), np.float32)/225
+##cv2.imshow('temp', temp)
+kernel = np.ones((20, 20), np.float32)/225
 smoothed = cv2.filter2D(edges, -1, kernel)
-cv2.imshow('smoothed', smoothed)
+#cv2.imshow('smoothed', smoothed)
 temp = smoothed
 temp2 = np.empty((30, 30), dtype=int)
 gs = 10
@@ -70,7 +106,7 @@ for j in range(n):
             temp[j * gs:(j + 1) * gs, i * gs:(i + 1) * gs] = 255
             temp2[j, i] = 0
 
-cv2.imshow('temp', temp)
+##cv2.imshow('temp', temp)
 g = open('grids.txt', 'w')
 g.write(st + "\n")
 np.savetxt("grids.txt", temp2)
@@ -98,7 +134,7 @@ g.close()
 #         else:
 #             temp1[j * gs:(j + 1) * gs, i * gs:(i + 1) * gs] = 255
 
-#cv2.imshow("grid", temp)
+##cv2.imshow("grid", temp)
 
 
 # src = result[b*gs:(b+1)*gs, a*gs:(a+1)*gs]
@@ -109,7 +145,7 @@ g.close()
 os.system("g++ dijk_path.cpp") #For Compiling
 os.system("./a.out")
 
-cv2.imshow("result", result)
+#cv2.imshow("result", result)
 outF = open('myOutFile.txt', 'w')
 theFile = open('srcdest.txt', 'r')
 arr = []
@@ -124,24 +160,24 @@ for i in range(l):
     p = v//30
     q = v % 30
     result[p * gs:(p + 1) * gs, q * gs:(q + 1) * gs, :] = [0, 255, 0]
-    print("l ")
+    print("l")
     temp[p * gs:(p + 1) * gs, q * gs:(q + 1) * gs] = 150
 
     if p == r+1:
-        print("Right")
-        outF.write('Right\n')
+        print("R")
+        outF.write('R\n')
 
     if q == s-1:
-        outF.write('Backward\n')
-        print("Backward")
+        outF.write('B\n')
+        print("B")
 
     if q == s+1:
-        outF.write('Forward\n')
-        print("Forward")
+        outF.write('F\n')
+        print("F")
 
     if p == r-1:
-        outF.write('Left\n')
-        print("Left")
+        outF.write('L\n')
+        print("L")
 
     print(p, q)
     print(r, s)
@@ -150,13 +186,40 @@ for i in range(l):
 
 theFile.close()
 outF.close()
+
+e_url = "192.168.0.112/specificArgs?direction="
+f = open('myOutFile.txt', 'r')
+a = " "
+c = 1
+for line in f:
+    for d in line.split():
+        if d != a and c == 1 and a == " ":
+            e_url = e_url + d
+        elif d != a:
+            e_url = e_url + str(c) + "+"
+            e_url = e_url + d
+            c = 1
+        elif d == a:
+            c += 1
+        a = d
+e_url = e_url + str(c)
+print(e_url)
+http = urllib3.PoolManager()
+
+response = http.request('GET', e_url)
+soup = BeautifulSoup(response.data)
+print(soup)
+
 cv2.imshow('path', result)
-cv2.imshow('pathed', temp)
+#cv2.imshow('pathed', temp)
+
 print("--- %s seconds ---" % (time.time() - start_time))
+#time.sleep(12000)
+
+
 
 while(True):
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
 #cap.release()
 cv2.destroyAllWindows()
